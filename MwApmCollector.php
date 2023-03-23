bc760ae558b0831e11fcac3966c2233a6ade54ff
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-03-23 12:44:35 +0530

    Middleware PHP-APM Agent

---
+++ MwApmCollector.php
@@ -0,0 +1,114 @@
+<?php
+declare(strict_types=1);
+namespace Middleware\AgentApmPhp;
+
+require 'vendor/autoload.php';
+
+use OpenTelemetry\Context\Context;
+use OpenTelemetry\API\Trace\StatusCode;
+use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
+use OpenTelemetry\Contrib\Otlp\SpanExporter;
+use OpenTelemetry\SDK\Common\Attribute\Attributes;
+use OpenTelemetry\SDK\Common\Configuration\Variables;
+use OpenTelemetry\SDK\Resource\ResourceInfo;
+use OpenTelemetry\Sdk\Trace\Span;
+use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
+use OpenTelemetry\SDK\Trace\TracerProvider;
+use OpenTelemetry\API\Trace\TracerInterface;
+use OpenTelemetry\SemConv\ResourceAttributes;
+
+final class MwApmCollector {
+
+    private int $exportPort = 9321;
+    private string $projectName;
+    private string $serviceName;
+    private TracerInterface $tracer;
+
+    public function __construct(string $projectName = null, string $serviceName = null) {
+
+        if (empty($projectName)) {
+            $projectName = 'Project-'. getmypid();
+        }
+
+        if (empty($serviceName)) {
+            $serviceName = 'Service-'. getmypid();
+        }
+
+        $this->projectName = $projectName;
+        $this->serviceName = $serviceName;
+
+        $transport = (new OtlpHttpTransportFactory())->create(
+            'http://localhost:' . $this->exportPort . '/v1/traces',
+            'application/x-protobuf');
+
+        $exporter = new SpanExporter($transport);
+
+        $tracerProvider = new TracerProvider(
+            new SimpleSpanProcessor($exporter),
+            null,
+            ResourceInfo::create(Attributes::create([
+                'project.name' => $projectName,
+                ResourceAttributes::SERVICE_NAME => $serviceName,
+                Variables::OTEL_PHP_AUTOLOAD_ENABLED => true,
+            ]))
+        );
+
+        // $this->tracer = $tracerProvider->getTracer('io.opentelemetry.contrib.php', '1.0.0');
+        $this->tracer = $tracerProvider->getTracer('middleware/agent-apm-php', 'dev-master');
+    }
+
+    public function preTracingCall(
+        ?string $className,
+        string $functionName,
+        ?string $fileName,
+        ?iterable $attributes = null): void {
+        $span = $this->tracer->spanBuilder(sprintf('%s::%s', $className, $functionName))
+            ->setAttribute('service.name', $this->serviceName)
+            ->setAttribute('project.name', $this->projectName)
+            ->setAttribute('code.function', $functionName)
+            ->setAttribute('code.namespace', $className)
+            ->setAttribute('code.filepath', $fileName);
+
+        if (!empty($attributes)) {
+            foreach ($attributes as $key => $value) {
+                $span->setAttribute($key, $value);
+            }
+        }
+        $span = $span->startSpan();
+
+        // $span = $tracer
+        //     ->spanBuilder(sprintf('%s %s', $request->getMethod(), $request->getUri()))
+        //     ->setSpanKind(SpanKind::KIND_CLIENT)
+        //     ->setAttribute('http.method', $request->getMethod())
+        //     ->setAttribute('http.url', $request->getUri())
+        //     ->startSpan();
+        // $span = $tracer
+        //     ->spanBuilder('get-user')
+        //     ->setAttribute('db.system', 'mysql')
+        //     ->setAttribute('db.name', 'users')
+        //     ->setAttribute('db.user', 'some_user')
+        //     ->setAttribute('db.statement', 'select * from users where username = :1')
+        //     ->startSpan();
+        Context::storage()->attach($span->storeInContext(Context::getCurrent()));
+    }
+
+    public function postTracingCall(): void {
+        if (!$scope = Context::storage()->scope()) {
+            return;
+        }
+
+        $scope->detach();
+        $span = Span::fromContext($scope->context());
+        $span->setStatus(StatusCode::STATUS_OK);
+        $span->end();
+    }
+
+    public function tracingCall(
+        ?string $className,
+        string $functionName,
+        ?string $fileName,
+        ?iterable $attributes = null): void {
+        $this->preTracingCall($className, $functionName, $fileName, $attributes);
+        $this->postTracingCall();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# agent-apm-php
-Agent APM for PHP
+# Getting Started
+
+### agent-apm-php
+Description: Agent APM for PHP
+
+### Guides
+To use this APM agent, you need to follow below steps:
+1. Run `code composer require middleware/agent-apm-php` in your project directory.
+2. After successful installation, you need to add `require 'vendor/autoload.php';` in your file.
+3. Then after, you need to add `use Middleware\AgentApmPhp\MwApmCollector;` line.
+4. Now, you need to add following code in your desired function of any class. This will collect the data, Like: called ClassName, FunctionName, TargetFile and send it to the Middleware Dashboard:
+
+    ```
+    $mwCollector = new MwApmCollector('<PROJECT-NAME>', '<SERVICE-NAME>');
+
+    $mwCollector->tracingCall(get_called_class(), __FUNCTION__, __FILE__);
+    ```
+5. You can add many other attributes as defined [here](https://opentelemetry.io/docs/reference/specification/trace/semantic_conventions/span-general/), or you can add your own custom attributes to the above code, like: 
+    
+    ```
+    $mwCollector->tracingCall(get_called_class(), __FUNCTION__, __FILE__, [
+         'code.lineno' => '10',
+         'code.column' => 12,
+         'net.host.name' => 'localhost',
+         'db.name' => 'users',
+         'custom.attr1' => 'value1',
+    ]);
+    ```
+### Sample Code
+```
+<?php
+require 'vendor/autoload.php';
+use Middleware\AgentApmPhp\MwApmCollector;
+
+class DoThings {
+    public static function printString($str): void {
+        echo $str . PHP_EOL;
+    }
+}
+
+class DemoClass {
+    public static function printFunction(): void {
+
+        $mwCollector = new MwApmCollector('DemoProject', 'PrintService');
+        $mwCollector->tracingCall(get_called_class(), __FUNCTION__, __FILE__, [
+            'code.lineno' => '10',
+            'code.column' => '12',
+            'net.host.name' => 'localhost',
+            'db.name' => 'users',
+            'custom.attr1' => 'value1',
+        ]);
+
+        DoThings::printString('Hello World!');
+
+    }
+}
+
+DemoClass::printFunction();
